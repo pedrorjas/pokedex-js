29f8b996d7b9e846278bf8fed9b43cd8850bf9f8
dev-pedro <dev.pedro.rjas@gmail.com>
2022-11-07 13:20:51 -0300

    Update do projeto Pokedex - 06/11/2022

---
+++ assets/js/main.js
@@ -0,0 +1,30 @@
+const pokemonList = document.getElementById('pokemonList');
+const loadMoreButton = document.getElementById('loadMoreButton');
+let offset = 0;
+const limit = 5;
+
+function loadPokemonsItens(offset, limit){
+    pokeApi.getPokemons(offset, limit).then((pokemons = []) => {
+        const newHtml = pokemons.map((pokemon) => `
+        <li class="pokemon ${pokemon.type}">
+                    <span class="number">#${pokemon.id}</span>
+                    <span class="name">${pokemon.name}</span>
+                    
+                    <div class="detail">
+                        <ol class="types">
+                        ${pokemon.types.map((type) => `<li class="type ${type}">${type}</li>`).join('')}
+                        </ol>
+                        <img src="${pokemon.photo}" alt="${pokemon.name}">
+                    </div>
+                    
+                </li>
+        `).join('');
+
+        pokemonList.innerHTML += newHtml;
+        })
+}
+
+loadMoreButton.addEventListener('click', () => {
+    loadPokemonsItens(offset, limit);
+    offset += limit;
+})
